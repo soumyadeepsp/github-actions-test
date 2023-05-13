4a0f319d8674757f144cba63c55beb663cc42395
soumyadeepsp <soumyadeepsp@gmail.com>
2023-05-13 09:09:11 +0530

    updated

--- cleanup.py
+++ cleanup.py
@@ -1,4 +1,25 @@
-f = open("branches.txt", "r")
-branches = f.read(0)
-f.close()
-print ("branches = "+branches)
+arr = []
+with open("branches.txt") as file:
+    for item in file:
+        print(item)
+        item = item.replace(' ', '')
+        item = item.replace('\n', '')
+        arr.append(item)
+print (arr)
+
+import subprocess
+test = subprocess.Popen(["git", "log", "origin/DASRE-1234", "--not", "master"], stdout=subprocess.PIPE)
+output = test.communicate()[0].decode()
+print (output.index('Date:'))
+start_index = output.index('Date:')+5
+end_index = output.index('\n\n')-6
+output = output[start_index:end_index]
+output = output.replace(' ', '')
+print (output)
+
+import datetime
+now = datetime.datetime.now()
+creation_time = datetime.datetime(2023, 5, 12, 1, 13, 9)
+print (creation_time)
+print (type(creation_time))
+print(now)
